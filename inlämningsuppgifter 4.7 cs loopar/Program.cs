fb94ba8cf145747de09153b211f38ccbdb9643ba
Admin <Admin@TEB123004>
2023-03-10 09:04:11 +0100

    Add project files.

---
+++ inlämningsuppgifter 4.7 cs loopar/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv in ett meddelande");
+            string mssg = Console.ReadLine();
+            Console.WriteLine("hur många rader vill du ska vara tomma");
+            int mell = int.Parse(Console.ReadLine());
+
+            for(int I=0; I<mssg.Length;I++)
+            {
+                for(int j=0; j<mell; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(mssg[I]);
+            }
+        }
+    }
+}
